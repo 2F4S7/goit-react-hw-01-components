d1c2790f56726d1163b743f926444d57f3ed3f63
Mikhail Prytychenko <nightsta1kerhf@gmail.com>
2021-06-02 16:35:41 +0300

    statistics-add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import user from './data/user.json';
+import statistics from './data/statistical.json';
 
 const App = () => {
   return (
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="stats" stats={statistics} />
+    </>
   );
 };
 
---
+++ src/Statistics/Statistics.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StatisticsItems from './StatisticsItems';
+import style from './Statistics.module.css';
+
+const Statistics = ({ stats, title }) => {
+  return (
+    <section className={style.statistics}>
+      {title && <h2 className={style.title}>{title}</h2>}
+      <ul className={style.statList}>
+        {stats.map(stat => (
+          <StatisticsItems key={stat.id} items={stat} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.string.isRequired }))
+    .isRequired,
+};
+
+export default Statistics;
