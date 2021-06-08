80365ef836a4bccd3f4d1a70a8e682852722db69
Mikhail Prytychenko <nightsta1kerhf@gmail.com>
2021-06-08 23:29:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import user from './data/user.json';
 import statistics from './data/statistical.json';
 
 const App = () => {
-  return (
-    <>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="stats" stats={statistics} />
-    </>
-  );
+    return (
+        <>
+            <Profile
+                name={user.name}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+            />
+            <Statistics title="stats" stats={statistics} />
+        </>
+    );
 };
 
 export default App;
